9a92b082e6910f7e34621a4cdae817e658041ce1
DavidMobileDeveloper <davidsilveirapinheiro@gmail.com>
2025-05-06 12:08:24 -0300

    Primeiro commit.

---
+++ src/main/kotlin/reforco/decisao/Decisao32.kt
@@ -0,0 +1,27 @@
+package reforco.decisao
+
+/*
+Enunciado:
+Crie um programa que receba a idade de uma pessoa e imprima "Você é maior de idade" se
+a idade for maior ou igual a 18.
+
+Passo a passo:
+
+Solicite ao usuário que insira sua idade.
+Armazene o valor da idade em uma variável.
+Use a estrutura if para verificar se a idade é maior ou igual a 18.
+Se a condição for verdadeira, imprima a mensagem "Você é maior de idade".
+Não é necessário tratar o caso contrário.
+
+ */
+
+fun main(){
+    print("Informe sua idade: ")
+    val idade = readln().toInt()
+
+    if(idade >= 18){
+        println("Você é maior de idade.")
+    }else{
+        println("Você é menor de idade.")
+    }
+}
