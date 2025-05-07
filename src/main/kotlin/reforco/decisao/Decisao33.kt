61feb270b4891ee0457fa71aaf4cc7e9af20a8da
DavidMobileDeveloper <davidsilveirapinheiro@gmail.com>
2025-05-07 11:55:28 -0300

    Decisao33.kt

---
+++ src/main/kotlin/reforco/decisao/Decisao33.kt
@@ -0,0 +1,25 @@
+package reforco.decisao
+
+/*
+Enunciado:
+Desenvolva um programa que verifique se um número fornecido pelo usuário é positivo ou negativo.
+
+Passo a passo:
+
+Solicite ao usuário que insira um número.
+Armazene o número em uma variável.
+Use a estrutura if..else para verificar se o número é maior ou igual a zero.
+Se a condição for verdadeira, imprima "O número é positivo".
+Caso contrário, imprima "O número é negativo".
+ */
+
+fun main(){
+    print("Informe um número inteiro qualquer: ")
+    val numero = readln().toInt()
+
+    if(numero >= 0){
+        println("O número informado é POSITIVO.")
+    }else{
+        println("O número informado é NEGATIVO.")
+    }
+}
